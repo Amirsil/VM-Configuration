44f1dbc8c12b5385a8515e0b6dfa18209fd70797
root <root@ddns.unixmen.local>
2021-10-28 05:26:43 -0400

    Add gitignore and idempotency

--- sync.sh
+++ sync.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
-# Apply all new changes to relevent machines in the inventory using GitOps.
-git pull origin main
-ansible-playbook configure.yaml
+### Apply all new changes to relevent machines in the inventory using GitOps.
+
+# If git repo is up to date, and the hosts configutation file
+# for ansible hasn't changed, nothing happens in order to achieve idempotency 
+if [[ $(git pull origin main 2>/dev/null) == *"up-to-date"* ]] && \
+	 [[ -z $(diff /etc/ansible/hosts .hosts 2>&1) ]]; then
+	echo Already Synced
+else	
+	ansible-playbook configure.yaml
+	cp /etc/ansible/hosts .hosts
+fi
