85522e5d12619320eae5ae6b58f5de6f7a13b9b8
Amirsil <57900754+Amirsil@users.noreply.github.com>
2021-11-02 21:31:27 +0200

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -5,7 +5,7 @@
 # If git repo is up to date, and the hosts configutation file
 # for ansible hasn't changed, nothing happens in order to achieve idempotency 
 if [[ $(git pull origin main 2>/dev/null) == *"up-to-date"* ]] && \
-	 [[ -z $(diff /etc/ansible/hosts .hosts 2>&1) ]]; then
+   [[ -z $(diff /etc/ansible/hosts .hosts 2>&1) ]]; then
 	echo Already Synced
 else	
 	ansible-playbook configure.yaml
